fb16852ed1a7beb1d138dbf06d018cd2639daeaf
dinhluan93 <ngodinhluan1@gmail.com>
2018-07-26 15:59:59 +0700

    Update APIs.js

--- APIs.js
+++ APIs.js
@@ -233,9 +233,11 @@ module.exports=function(app){
 			
 			var setStart = phaseBonus.methods.setPhaseSale(req.body.phase,0,req.body.startDate).encodeABI(),
 			setEnd = phaseBonus.methods.setPhaseSale(req.body.phase,1,req.body.endDate).encodeABI();
-			sendSignedTransaction(results[0].OwnerAddress,results[0].Address, descryptionPrivateKey(results[0].PrivateKey), setStart);
-			sendSignedTransaction(results[0].OwnerAddress,results[0].Address, descryptionPrivateKey(results[0].PrivateKey), setEnd);
-			res.send(helper.response(statusCode,message,true));
+			new Promise(async (resolve, reject) => {
+			 await sendSignedTransaction(results[0].OwnerAddress,results[0].Address, descryptionPrivateKey(results[0].PrivateKey), setStart);
+			 sendSignedTransaction(results[0].OwnerAddress,results[0].Address, descryptionPrivateKey(results[0].PrivateKey), setEnd);
+				res.send(helper.response(statusCode,message,true));
+			});
 		});
 	});
 	app.post('/api/setBonus', function(req, res){
